8627c7137e42226820802136bd9e5380e74a8c92
Cvayoyo <145971481+Cvayoyo@users.noreply.github.com>
2024-04-07 11:19:00 +0700

    Update ssh-verus.sh

--- ssh-verus.sh
+++ ssh-verus.sh
@@ -1,1 +1,26 @@
-gcloud compute networks create dm-stamford --subnet-mode=custom && gcloud compute networks subnets create dm-stamford-us-east4 --range=172.21.0.0/24 --network=dm-stamford --region=us-east4 && gcloud compute networks subnets create dm-stamford-us-east1 --range=162.21.0.0/24 --network=dm-stamford --region=us-east1 && PROJECT_ID=$(gcloud config get-value core/project) && gcloud compute instances create instance-1 instance-2 instance-3 instance-4 instance-5 --zone=us-east4-c --machine-type=e2-standard-4 --network=dm-stamford --subnet=dm-stamford-us-east4 --metadata=startup-script=apt\ \install\ git\ -y$'\n'git\ clone\ https://github.com/Cvayoyo/ario_verus\ \&\&\ cd\ ario_verus$'\n'chmod\ \+x\ \*$'\n'./install.sh --maintenance-policy=MIGRATE --provisioning-model=STANDARD --service-account=$PROJECT_ID@$PROJECT_ID.iam.gserviceaccount.com --scopes=https://www.googleapis.com/auth/cloud-platform --create-disk=auto-delete=yes,boot=yes,device-name=instance-1,image=projects/ubuntu-os-cloud/global/images/ubuntu-2204-jammy-v20240319,mode=rw,size=10,type=projects/$PROJECT_ID/zones/us-east4-c/diskTypes/pd-balanced --reservation-affinity=any && gcloud compute instances create instance-6 --zone=us-east1-c --machine-type=e2-standard-4 --network=dm-stamford --subnet=dm-stamford-us-east1 --metadata=startup-script=apt\ \install\ git\ -y$'\n'git\ clone\ https://github.com/Cvayoyo/ario_verus\ \&\&\ cd\ ario_verus$'\n'chmod\ \+x\ \*$'\n'./install.sh --maintenance-policy=MIGRATE --provisioning-model=STANDARD --service-account=$PROJECT_ID@$PROJECT_ID.iam.gserviceaccount.com --scopes=https://www.googleapis.com/auth/cloud-platform --create-disk=auto-delete=yes,boot=yes,device-name=instance-1,image=projects/ubuntu-os-cloud/global/images/ubuntu-2204-jammy-v20240319,mode=rw,size=10,type=projects/$PROJECT_ID/zones/us-east1-c/diskTypes/pd-balanced --reservation-affinity=any && exit
+generate_random_name() {
+    vowels=("a" "e" "i" "o" "u")
+    consonants=("b" "c" "d" "f" "g" "h" "j" "k" "l" "m" "n" "p" "q" "r" "s" "t" "v" "w" "x" "y" "z")
+
+    # Random length between 3 and 8 characters
+    name_length=$((20 % 6 + 3))
+
+    name=""
+
+    for ((i=0; i<20; i++)); do
+        # Alternate between vowel and consonant
+        if (( i % 2 == 0 )); then
+            rand_vowel=${vowels[$RANDOM % ${#vowels[@]}]}
+            name="${name}${rand_vowel}"
+        else
+            rand_consonant=${consonants[$RANDOM % ${#consonants[@]}]}
+            name="${name}${rand_consonant}"
+        fi
+    done
+
+    echo "$name"
+}
+STATIC_IP_NAME=$(generate_random_name)
+STATIC_IP_NAME1=$(generate_random_name)
+STATIC_IP_NAME2=$(generate_random_name)
+gcloud compute networks create $STATIC_IP_NAME --subnet-mode=custom && gcloud compute networks subnets create $STATIC_IP_NAME1 --range=172.21.0.0/24 --network=$STATIC_IP_NAME --region=us-east4 && gcloud compute networks subnets create $STATIC_IP_NAME2 --range=162.21.0.0/24 --network=$STATIC_IP_NAME --region=us-east1 && PROJECT_ID=$(gcloud config get-value core/project) && gcloud compute instances create instance-1 instance-2 instance-3 instance-4 instance-5 --zone=us-east4-c --machine-type=e2-standard-4 --network=$STATIC_IP_NAME --subnet=$STATIC_IP_NAME1 --metadata=startup-script=apt\ \install\ git\ -y$'\n'git\ clone\ https://github.com/Cvayoyo/ario_verus\ \&\&\ cd\ ario_verus$'\n'chmod\ \+x\ \*$'\n'./install.sh --maintenance-policy=MIGRATE --provisioning-model=STANDARD --service-account=$PROJECT_ID@$PROJECT_ID.iam.gserviceaccount.com --scopes=https://www.googleapis.com/auth/cloud-platform --create-disk=auto-delete=yes,boot=yes,device-name=instance-1,image=projects/ubuntu-os-cloud/global/images/ubuntu-2204-jammy-v20240319,mode=rw,size=10,type=projects/$PROJECT_ID/zones/us-east4-c/diskTypes/pd-balanced --reservation-affinity=any && gcloud compute instances create instance-6 --zone=us-east1-c --machine-type=e2-standard-4 --network=$STATIC_IP_NAME --subnet=$STATIC_IP_NAME2 --metadata=startup-script=apt\ \install\ git\ -y$'\n'git\ clone\ https://github.com/Cvayoyo/ario_verus\ \&\&\ cd\ ario_verus$'\n'chmod\ \+x\ \*$'\n'./install.sh --maintenance-policy=MIGRATE --provisioning-model=STANDARD --service-account=$PROJECT_ID@$PROJECT_ID.iam.gserviceaccount.com --scopes=https://www.googleapis.com/auth/cloud-platform --create-disk=auto-delete=yes,boot=yes,device-name=instance-1,image=projects/ubuntu-os-cloud/global/images/ubuntu-2204-jammy-v20240319,mode=rw,size=10,type=projects/$PROJECT_ID/zones/us-east1-c/diskTypes/pd-balanced --reservation-affinity=any && exit
