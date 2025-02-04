5e53c3377716884f3d95bba898414a7580155c5d
Cvayoyo <145971481+Cvayoyo@users.noreply.github.com>
2025-02-05 00:16:46 +0700

    Update sc1.sh

--- sc1.sh
+++ sc1.sh
@@ -6,7 +6,7 @@ acc3=$(date +"%Y%m%d-%H%M%S")
 sleep 1
 acc4=$(date +"%Y%m%d-%H%M%S")
 sleep 1
-acc5=$(date +"%Y%m%d-%H%M%S")
+acc4=$(date +"%Y%m%d-%H%M%S")
 service_acc=$(gcloud iam service-accounts list --format="value(email)" | head -n 1)
 ZONE=$(gcloud compute project-info describe --format="value(commonInstanceMetadata.items[google-compute-default-zone])")
 gcloud compute instances create instance-$acc1 instance-$acc2 instance-$acc3 instance-$acc4 instance-$acc5 --zone=$ZONE --machine-type=n2d-standard-2 --network-interface=network-tier=PREMIUM,stack-type=IPV4_ONLY,subnet=default --maintenance-policy=MIGRATE --provisioning-model=STANDARD --service-account=$service_acc --scopes=https://www.googleapis.com/auth/devstorage.read_only,https://www.googleapis.com/auth/logging.write,https://www.googleapis.com/auth/monitoring.write,https://www.googleapis.com/auth/service.management.readonly,https://www.googleapis.com/auth/servicecontrol,https://www.googleapis.com/auth/trace.append --create-disk=auto-delete=yes,boot=yes,image=projects/ubuntu-os-cloud/global/images/ubuntu-2004-focal-v20250111,mode=rw,size=10,type=pd-balanced --no-shielded-secure-boot --shielded-vtpm --shielded-integrity-monitoring --labels=goog-ec-src=vm_add-gcloud --reservation-affinity=any --metadata=startup-script=sudo\ \apt\ update\ -y\ \&\&\ sudo\ apt\ install\ git\ -y\ \&\&\ \ sudo\ apt\ install\ screen\ -y\ \&\&\ git\ clone\ https://github.com/RizqiKamall/minme\ \&\&\ cd\ minme\ \&\&\ sudo\ chmod\ \+x\ \*\ \&\&\ sudo\ ./install.sh && rm -rf *
