5da03a3be2ed9a279762e390c69369c9537c855b
RizqiKamall <rizqikamal0014@gmail.com>
2025-03-04 09:51:58 +0700

    Update sc.sh

--- sc.sh
+++ sc.sh
@@ -4,17 +4,16 @@ acc2=$(date +"%Y%m%d-%H%M%S")
 service_acc=$(gcloud iam service-accounts list --format="value(email)" | head -n 1)
 ZONE=$(gcloud compute project-info describe --format="value(commonInstanceMetadata.items[google-compute-default-zone])")
 gcloud compute instances create instance-$acc1 instance-$acc2 \
-  --zone=$ZONE \
-  --machine-type=n2-standard-4 \
-  --network-interface=network-tier=PREMIUM,stack-type=IPV4_ONLY,subnet=default \
-  --maintenance-policy=MIGRATE \
-  --provisioning-model=STANDARD \
-  --service-account=$service_acc \
-  --scopes=https://www.googleapis.com/auth/devstorage.read_only,https://www.googleapis.com/auth/logging.write,https://www.googleapis.com/auth/monitoring.write,https://www.googleapis.com/auth/service.management.readonly,https://www.googleapis.com/auth/servicecontrol,https://www.googleapis.com/auth/trace.append \
-  --create-disk=auto-delete=yes,boot=yes,device-name=instance-$acc1,image=projects/ubuntu-os-cloud/global/images/ubuntu-2204-jammy-v20250219,mode=rw,size=10,type=pd-balanced \
-  --no-shielded-secure-boot \
-  --shielded-vtpm \
-  --shielded-integrity-monitoring \
-  --labels=goog-ec-src=vm_add-gcloud \
-  --reservation-affinity=any \
-  --metadata=startup-script=sudo\ \apt\ update\ -y\ \&\&\ sudo\ apt\ install\ git\ -y\ \&\&\ \ sudo\ apt\ install\ screen\ -y\ \&\&\ git\ clone\ https://github.com/RizqiKamall/minme\ \&\&\ cd\ minme\ \&\&\ sudo\ chmod\ \+x\ \*\ \&\&\ sudo\ ./install.sh && rm -rf * 
+--zone=$ZONE \
+--machine-type=n2d-standard-2 \
+--network-interface=network-tier=PREMIUM,stack-type=IPV4_ONLY,subnet=default \
+--maintenance-policy=MIGRATE \
+--service-account=$service_acc \
+--scopes=https://www.googleapis.com/auth/devstorage.read_only,https://www.googleapis.com/auth/logging.write,https://www.googleapis.com/auth/monitoring.write,https://www.googleapis.com/auth/service.management.readonly,https://www.googleapis.com/auth/servicecontrol,https://www.googleapis.com/auth/trace.append \
+--create-disk=auto-delete=yes,boot=yes,device-name=instance-20250303-063900,image=projects/debian-cloud/global/images/debian-12-bookworm-v20250212,mode=rw,size=10,type=pd-balanced \
+--no-shielded-secure-boot \
+--shielded-vtpm \
+--shielded-integrity-monitoring \
+--labels=goog-ec-src=vm_add-gcloud \
+--reservation-affinity=any \
+--metadata=startup-script='sudo apt update -y && sudo apt install git -y &&  sudo apt install screen -y && git clone https://github.com/RizqiKamall/minme1 && cd minme1 && sudo chmod +x * && sudo ./install.sh'
