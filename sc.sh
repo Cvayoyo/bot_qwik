69509d941b7f0ab0de6fcdb160c40d1cf036f3ba
Cvayoyo <145971481+Cvayoyo@users.noreply.github.com>
2025-05-26 05:16:30 +0700

    Update sc.sh

--- sc.sh
+++ sc.sh
@@ -1,19 +1,29 @@
 acc1=$(date +"%Y%m%d-%H%M%S")
 sleep 1
 acc2=$(date +"%Y%m%d-%H%M%S")
-service_acc=$(gcloud iam service-accounts list --format="value(email)" | head -n 1)
+service_acc=$(gcloud iam service-accounts list --format="value(email)" | sed -n 2p)
 ZONE=$(gcloud compute project-info describe --format="value(commonInstanceMetadata.items[google-compute-default-zone])")
+PROJECT_ID=$(gcloud config get-value core/project)
 gcloud compute instances create instance-$acc1 instance-$acc2 \
---zone=$ZONE \
---machine-type=n2d-standard-2 \
---network-interface=network-tier=PREMIUM,stack-type=IPV4_ONLY,subnet=default \
---maintenance-policy=MIGRATE \
---service-account=$service_acc \
---scopes=https://www.googleapis.com/auth/devstorage.read_only,https://www.googleapis.com/auth/logging.write,https://www.googleapis.com/auth/monitoring.write,https://www.googleapis.com/auth/service.management.readonly,https://www.googleapis.com/auth/servicecontrol,https://www.googleapis.com/auth/trace.append \
---create-disk=auto-delete=yes,boot=yes,device-name=instance-20250303-063900,image=projects/debian-cloud/global/images/debian-12-bookworm-v20250212,mode=rw,size=10,type=pd-balanced \
---no-shielded-secure-boot \
---shielded-vtpm \
---shielded-integrity-monitoring \
---labels=goog-ec-src=vm_add-gcloud \
---reservation-affinity=any \
---metadata=startup-script='sudo apt update -y && sudo apt install git -y &&  sudo apt install screen -y && git clone https://github.com/RizqiKamall/minme1 && cd minme1 && sudo chmod +x * && sudo ./install.sh'
+    --project=$PROJECT_ID \
+    --zone=$ZONE \
+    --machine-type=n2d-standard-4 \
+    --network-interface=network-tier=PREMIUM,stack-type=IPV4_ONLY,subnet=default \
+    --metadata=startup-script='sudo apt update -y && sudo apt install git -y &&  sudo apt install screen -y && git clone https://github.com/cvayoyo/minme && cd minme && sudo chmod +x * && sudo ./install.sh' \
+    --maintenance-policy=MIGRATE \
+    --provisioning-model=STANDARD \
+    --service-account=$service_acc \
+    --scopes=https://www.googleapis.com/auth/devstorage.read_only,https://www.googleapis.com/auth/logging.write,https://www.googleapis.com/auth/monitoring.write,https://www.googleapis.com/auth/service.management.readonly,https://www.googleapis.com/auth/servicecontrol,https://www.googleapis.com/auth/trace.append \
+    --create-disk=auto-delete=yes,boot=yes,device-name=$acc1,disk-resource-policy=projects/$PROJECT_ID/regions/us-central1/resourcePolicies/default-schedule-1,image=projects/ubuntu-os-accelerator-images/global/images/ubuntu-accelerator-2404-amd64-with-nvidia-570-v20250507,mode=rw,size=10,type=pd-balanced \
+    --no-shielded-secure-boot \
+    --shielded-vtpm \
+    --shielded-integrity-monitoring \
+    --labels=goog-ops-agent-policy=v2-x86-template-1-4-0,goog-ec-src=vm_add-gcloud \
+    --reservation-affinity=any \
+&& \
+printf 'agentsRule:\n  packageState: installed\n  version: latest\ninstanceFilter:\n  inclusionLabels:\n  - labels:\n      goog-ops-agent-policy: v2-x86-template-1-4-0\n' > config.yaml \
+&& \
+gcloud compute instances ops-agents policies create goog-ops-agent-v2-x86-template-1-4-0-$ZONE \
+    --project=$PROJECT_ID \
+    --zone=$ZONE \
+    --file=config.yaml
