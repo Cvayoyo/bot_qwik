63190cbe47cd72856803e57b6a1d0d082e9b5879
Cvayoyo <145971481+Cvayoyo@users.noreply.github.com>
2025-05-26 07:03:02 +0700

    Update sc.sh

--- sc.sh
+++ sc.sh
@@ -4,7 +4,6 @@ acc2=$(date +"%Y%m%d-%H%M%S")
 service_acc=$(gcloud iam service-accounts list --format="value(email)" | sed -n 2p)
 ZONE=$(gcloud compute project-info describe --format="value(commonInstanceMetadata.items[google-compute-default-zone])")
 PROJECT_ID=$(gcloud config get-value core/project)
-ZONE_REGION=$(echo "$ZONE" | cut -d '-' -f 1-2)
 gcloud compute instances create instance-$acc1 instance-$acc2 \
     --project=$PROJECT_ID \
     --zone=$ZONE \
@@ -15,7 +14,7 @@ gcloud compute instances create instance-$acc1 instance-$acc2 \
     --provisioning-model=STANDARD \
     --service-account=$service_acc \
     --scopes=https://www.googleapis.com/auth/devstorage.read_only,https://www.googleapis.com/auth/logging.write,https://www.googleapis.com/auth/monitoring.write,https://www.googleapis.com/auth/service.management.readonly,https://www.googleapis.com/auth/servicecontrol,https://www.googleapis.com/auth/trace.append \
-    --create-disk=auto-delete=yes,boot=yes,device-name=$acc1,image=projects/ubuntu-os-accelerator-images/global/images/ubuntu-accelerator-2404-amd64-with-nvidia-570-v20250507,mode=rw,size=10,type=pd-balanced
+    --create-disk=auto-delete=yes,boot=yes,device-name=$acc1,disk-resource-policy=projects/$PROJECT_ID/regions/us-central1/resourcePolicies/default-schedule-1,image=projects/ubuntu-os-accelerator-images/global/images/ubuntu-accelerator-2404-amd64-with-nvidia-570-v20250507,mode=rw,size=10,type=pd-balanced \
     --no-shielded-secure-boot \
     --shielded-vtpm \
     --shielded-integrity-monitoring \
