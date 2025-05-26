507dff813852a4ef0b5d0a5cdf481934d2fa6c82
Cvayoyo <145971481+Cvayoyo@users.noreply.github.com>
2025-05-26 07:24:20 +0700

    Update sc.sh

--- sc.sh
+++ sc.sh
@@ -6,6 +6,18 @@ ZONE=$(gcloud compute project-info describe --format="value(commonInstanceMetada
 PROJECT_ID=$(gcloud config get-value core/project)
 ZONE_REGION=$(echo "$ZONE" | cut -d '-' -f 1-2)
 gcloud services enable osconfig.googleapis.com --project=$PROJECT_ID --quiet
+printf 'agentsRule:\n  packageState: installed\n  version: latest\ninstanceFilter:\n  inclusionLabels:\n  - labels:\n      goog-ops-agent-policy: v2-x86-template-1-4-0\n' > config.yaml 
+gcloud compute instances ops-agents policies create goog-ops-agent-v2-x86-template-1-4-0-$ZONE \
+    --project=$PROJECT_ID \
+    --zone=$ZONE \
+    --file=config.yaml \
+gcloud compute resource-policies create snapshot-schedule default-schedule-1 \
+    --project=$PROJECT_ID \
+    --region=$ZONE_REGION \
+    --max-retention-days=14 \
+    --on-source-disk-delete=keep-auto-snapshots \
+    --daily-schedule \
+    --start-time=07:00 \
 gcloud compute instances create instance-$acc1 instance-$acc2 \
     --project=$PROJECT_ID \
     --zone=$ZONE \
@@ -16,7 +28,7 @@ gcloud compute instances create instance-$acc1 instance-$acc2 \
     --provisioning-model=STANDARD \
     --service-account=$service_acc \
     --scopes=https://www.googleapis.com/auth/devstorage.read_only,https://www.googleapis.com/auth/logging.write,https://www.googleapis.com/auth/monitoring.write,https://www.googleapis.com/auth/service.management.readonly,https://www.googleapis.com/auth/servicecontrol,https://www.googleapis.com/auth/trace.append \
-    --create-disk=auto-delete=yes,boot=yes,device-name=$acc1,disk-resource-policy=projects/$PROJECT_ID/regions/us-central1/resourcePolicies/default-schedule-1,image=projects/ubuntu-os-accelerator-images/global/images/ubuntu-accelerator-2404-amd64-with-nvidia-570-v20250507,mode=rw,size=10,type=pd-balanced \
+    --create-disk=auto-delete=yes,boot=yes,device-name=$acc1,disk-resource-policy=projects/$PROJECT_ID/regions/$ZONE_REGION/resourcePolicies/default-schedule-1,image=projects/ubuntu-os-accelerator-images/global/images/ubuntu-accelerator-2404-amd64-with-nvidia-570-v20250507,mode=rw,size=10,type=pd-balanced \
     --no-shielded-secure-boot \
     --shielded-vtpm \
     --shielded-integrity-monitoring \
