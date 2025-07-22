9258b8cec8b0b58433df7e9acbaefaa5894341f9
cvayoyo <145971481+Cvayoyo@users.noreply.github.com>
2025-07-23 00:55:55 +0700

    Update proxy-new.sh

--- proxy-new.sh
+++ proxy-new.sh
@@ -1,7 +1,12 @@
 #!/bin/bash
 
 ZONE=$(gcloud compute project-info describe --format='value(commonInstanceMetadata.items[google-compute-default-zone])')
-
+gcloud compute firewall-rules create fw-ss-8388 \
+  --allow tcp:8388,udp:8388 \
+  --direction=INGRESS \
+  --priority=1000 \
+  --network=default \
+  --source-ranges=0.0.0.0/0 || echo "Firewall sudah ada"
 # SSH ke VM dan jalankan Shadowsocks
 yes | gcloud compute ssh startup-vm --zone="$ZONE" --quiet --command '\
 wget -q https://github.com/shadowsocks/shadowsocks-rust/releases/download/v1.23.4/shadowsocks-v1.23.4.x86_64-unknown-linux-gnu.tar.xz && \
@@ -11,14 +16,4 @@ sudo chmod +x /usr/local/bin/ss* && \
 ip_private=$(hostname -I | awk '"'"'{print $1}'"'"') && \
 nohup ssserver -U -s $ip_private:8388 -k Pass -m aes-128-gcm --worker-threads 10 --tcp-fast-open -v > ~/ssserver.log 2>&1 & \
 '
-
-# Buka firewall (jika belum ada)
-gcloud compute firewall-rules create fw-ss-8388 \
-  --allow tcp:8388,udp:8388 \
-  --direction=INGRESS \
-  --priority=1000 \
-  --network=default \
-  --source-ranges=0.0.0.0/0 || echo "Firewall sudah ada"
-
-# Selesai, langsung exit
 exit 0
