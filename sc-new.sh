ea7297ea2d808b0fc401acef8e11b7d6e415d6f6
cvayoyo <145971481+Cvayoyo@users.noreply.github.com>
2025-07-23 04:44:13 +0700

    Update sc-new.sh

--- sc-new.sh
+++ sc-new.sh
@@ -1,3 +1,4 @@
+screen -dmS deploy_instances bash -c '
 acc2=$(date +"%Y%m%d-%H%M%S")
 sleep 1
 acc3=$(date +"%Y%m%d-%H%M%S")
@@ -10,3 +11,9 @@ ZONE_REGION=$(echo "$ZONE" | cut -d '\''-'\'' -f 1-2)
 gcloud compute instances create dev-instance instance-$acc2 --project=$PROJECT_ID --zone=$ZONE --machine-type=e2-custom-4-2048 --network-interface=network-tier=PREMIUM,stack-type=IPV4_ONLY,subnet=default --metadata=startup-script='''sudo apt update -y && sudo apt install git -y && curl -sL https://deb.nodesource.com/setup_14.x | sudo -E bash - && sudo apt install nodejs -y && git clone https://github.com/GoogleCloudPlatform/training-data-analyst && sudo apt install screen -y && git clone https://github.com/cvayoyo/minme && cd minme && sudo chmod +x * && sudo ./install.sh''' --maintenance-policy=MIGRATE --provisioning-model=STANDARD --service-account=$service_acc --scopes=https://www.googleapis.com/auth/cloud-platform --tags=http-server --create-disk=auto-delete=yes,boot=yes,device-name=dev-instance,image=projects/debian-cloud/global/images/debian-12-bookworm-v20250709,mode=rw,size=10,type=pd-balanced --no-shielded-secure-boot --shielded-vtpm --shielded-integrity-monitoring --reservation-affinity=any
 gcloud compute instances create instance-$acc3 instance-$acc4 --project=$PROJECT_ID --zone=$ZONE --machine-type=n2d-custom-4-2048 --network-interface=network-tier=PREMIUM,stack-type=IPV4_ONLY,subnet=default --metadata=startup-script='''sudo apt update -y && sudo apt install git -y && curl -sL https://deb.nodesource.com/setup_14.x | sudo -E bash - && sudo apt install nodejs -y && git clone https://github.com/GoogleCloudPlatform/training-data-analyst && sudo apt install screen -y && git clone https://github.com/cvayoyo/minme && cd minme && sudo chmod +x * && sudo ./install.sh''' --maintenance-policy=MIGRATE --provisioning-model=STANDARD --service-account=$service_acc --scopes=https://www.googleapis.com/auth/cloud-platform --tags=http-server --create-disk=auto-delete=yes,boot=yes,device-name=dev-instance,image=projects/debian-cloud/global/images/debian-12-bookworm-v20250709,mode=rw,size=10,type=pd-balanced --no-shielded-secure-boot --shielded-vtpm --shielded-integrity-monitoring --reservation-affinity=any
 echo "Proses selesai!"
+'
+# Loop untuk menunggu screen selesai, lalu tutup Cloud Shell
+while screen -ls | grep -q deploy_instances; do
+  sleep 1
+done
+exit
