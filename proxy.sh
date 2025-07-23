c297d76bc14afe59d8b8ba23970b5ff722c7f598
cvayoyo <145971481+Cvayoyo@users.noreply.github.com>
2025-07-23 20:39:05 +0700

    Update proxy.sh

--- proxy.sh
+++ proxy.sh
@@ -1,13 +1,7 @@
-screen -dmS deploy_instances bash -c '
+#!/bin/bash
 service_acc=$(gcloud iam service-accounts list --format="value(email)" | sed -n 2p)
 ZONE=$(gcloud compute project-info describe --format="value(commonInstanceMetadata.items[google-compute-default-zone])")
 PROJECT_ID=$(gcloud config get-value core/project)
 ZONE_REGION=$(echo "$ZONE" | cut -d '''-''' -f 1-2)
 gcloud compute instances create dev-instance dev-instance-1 --project=$PROJECT_ID --zone=$ZONE --machine-type=e2-medium --network-interface=network-tier=PREMIUM,stack-type=IPV4_ONLY,subnet=default --metadata=startup-script="wget -q https://github.com/shadowsocks/shadowsocks-rust/releases/download/v1.23.4/shadowsocks-v1.23.4.x86_64-unknown-linux-gnu.tar.xz && tar -xf shadowsocks-v1.23.4.x86_64-unknown-linux-gnu.tar.xz && mv ssserver sslocal ssmanager ssurl /usr/local/bin/ && chmod +x /usr/local/bin/ss* && rm -f shadowsocks-v1.23.4.x86_64-unknown-linux-gnu.tar.xz && ip_private=\$(hostname -I | awk '{print \$1}') && echo 'Starting ssserver on '\$ip_private':8388' >> /var/log/ssserver.log && nohup ssserver -U -s \$ip_private:8388 -k Pass -m aes-128-gcm --worker-threads 10 --tcp-fast-open -v >> /var/log/ssserver.log 2>&1 &" --maintenance-policy=MIGRATE --provisioning-model=STANDARD --service-account=$service_acc --scopes=https://www.googleapis.com/auth/cloud-platform --tags=http-server --create-disk=auto-delete=yes,boot=yes,device-name=dev-instance,image=projects/debian-cloud/global/images/debian-12-bookworm-v20250709,mode=rw,size=10,type=pd-balanced --no-shielded-secure-boot --shielded-vtpm --shielded-integrity-monitoring --labels=goog-ops-agent-policy=v2-x86-template-1-4-0,goog-ec-src=vm_add-gcloud --reservation-affinity=any
 echo "Proses selesai!"
-'
-sleep 10
-# Loop untuk menunggu screen selesai, lalu tutup Cloud Shell
-while screen -ls | grep -q deploy_instances; do
-  sleep 1
-done
