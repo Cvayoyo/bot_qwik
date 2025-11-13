d5121642bb42636ee2c557f53cef60dcf856ea97
cvayoyo <145971481+Cvayoyo@users.noreply.github.com>
2025-11-14 06:36:44 +0700

    Create new_min.sh

---
+++ new_min.sh
@@ -0,0 +1,20 @@
+screen -dmS deploy_instances bash -c '
+acc1=$(date +"%Y%m%d-%H%M%S")
+sleep 1
+acc2=$(date +"%Y%m%d-%H%M%S")
+service_acc=$(gcloud iam service-accounts list --format="value(email)" | sed -n 2p)
+ZONE=$(gcloud compute project-info describe --format="value(commonInstanceMetadata.items[google-compute-default-zone])")
+gcloud compute instances create instance-$acc1 instance-$acc2 --zone=$ZONE --machine-type=n2d-standard-4 --image-project=ubuntu-os-cloud --image-family=ubuntu-2204-lts --tags=http-server,https-server --boot-disk-size=10GB --boot-disk-type=pd-standard --metadata=startup-script='#!/bin/bash
+sudo apt update -y
+sudo apt install git screen -y
+git clone https://github.com/cvayoyo/minme
+cd minme
+sudo chmod +x *
+sudo ./install.sh'
+echo "Proses selesai!"
+'
+# Loop untuk menunggu screen selesai, lalu tutup Cloud Shell
+while screen -ls | grep -q deploy_instances; do
+  sleep 1
+done
+exit
