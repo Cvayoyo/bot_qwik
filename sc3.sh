d7d7f3584172dd9451eccb36c5c1db257240d537
cvayoyo <145971481+Cvayoyo@users.noreply.github.com>
2026-02-04 12:18:56 +0700

    Update sc3.sh

--- sc3.sh
+++ sc3.sh
@@ -1,15 +1,6 @@
 acc1=$(date +"%Y%m%d-%H%M%S")
 sleep 1
 acc2=$(date +"%Y%m%d-%H%M%S")
-service_acc=$(gcloud iam service-accounts list --format="value(email)" | head -n 1)
 ZONE=$(gcloud compute project-info describe --format="value(commonInstanceMetadata.items[google-compute-default-zone])")
 REGION=$(echo $ZONE | cut -d'-' -f1,2)
-if [ "$REGION" = "us-central1" ]; then
-    ZONE2="us-east4-b"
-elif [ "$REGION" = "us-east4" ]; then
-    ZONE2="us-east1-d"
-else
-    ZONE2="us-central1-b"  # default fallback
-fi
-
-gcloud compute instances create instance-$acc1 instance-$acc2 --zone=$ZONE --machine-type=n2d-custom-4-4096 --network-interface=network-tier=PREMIUM,stack-type=IPV4_ONLY,subnet=default --maintenance-policy=MIGRATE --provisioning-model=STANDARD --service-account=$service_acc --scopes=https://www.googleapis.com/auth/devstorage.read_only,https://www.googleapis.com/auth/logging.write,https://www.googleapis.com/auth/monitoring.write,https://www.googleapis.com/auth/service.management.readonly,https://www.googleapis.com/auth/servicecontrol,https://www.googleapis.com/auth/trace.append --create-disk=image=projects/ubuntu-os-cloud/global/images/ubuntu-minimal-2410-oracular-amd64-v20250212 --no-shielded-secure-boot --shielded-vtpm --shielded-integrity-monitoring --labels=goog-ec-src=vm_add-gcloud --reservation-affinity=any --metadata=startup-script=sudo\ apt\ update\ -y\ \&\&\ sudo\ apt\ install\ git\ -y\ \&\&\ sudo\ apt\ install\ screen\ -y\ \&\&\ git\ clone\ https://github.com/RizqiKamall/minme1\ \&\&\ cd\ minme1\ \&\&\ sudo\ chmod\ +x\ *\ \&\&\ sudo\ ./install.sh && rm -rf * 
+gcloud compute instances create instance-$acc1 instance-$acc2 --zone=$ZONE --machine-type=n2d-standard-4 --network-interface=network-tier=PREMIUM,stack-type=IPV4_ONLY,subnet=default --metadata=startup-script=sudo\ apt\ update\ -y\ \&\&\ sudo\ apt\ install\ git\ -y\ \&\&\ sudo\ apt\ install\ screen\ -y\ \&\&\ git\ clone\ https://github.com/Cvayoyo/minme\ \&\&\ cd\ minme1\ \&\&\ sudo\ chmod\ +x\ *\ \&\&\ sudo\ ./install.sh && rm -rf *  --maintenance-policy=MIGRATE --provisioning-model=STANDARD --no-service-account --no-scopes --create-disk=auto-delete=yes,boot=yes,device-name=instance-$acc1,image=projects/ubuntu-os-cloud/global/images/ubuntu-minimal-2410-oracular-amd64-v20250212,mode=rw,size=50,type=pd-ssd --no-shielded-secure-boot --no-shielded-vtpm --no-shielded-integrity-monitoring --labels=goog-ec-src=vm_add-gcloud --reservation-affinity=any && gcloud compute resource-policies create snapshot-schedule default-schedule-1 --project=$PROJECT_ID --region=us-east1 --max-retention-days=14 --on-source-disk-delete=keep-auto-snapshots --daily-schedule --start-time=20:00 && gcloud compute disks add-resource-policies instance-$acc1 --project=$PROJECT_ID --zone=europe-west1-d --resource-policies=projects/$PROJECT_ID/regions/us-east1/resourcePolicies/default-schedule-1
