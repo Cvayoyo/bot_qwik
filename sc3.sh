a78958637872ac53291e942822925eb54e23b990
cvayoyo <145971481+Cvayoyo@users.noreply.github.com>
2026-02-05 04:48:22 +0700

    Update sc3.sh

--- sc3.sh
+++ sc3.sh
@@ -7,4 +7,4 @@ sleep 1
 acc4=$(date +"%Y%m%d-%H%M%S")
 ZONE=$(gcloud compute project-info describe --format="value(commonInstanceMetadata.items[google-compute-default-zone])")
 REGION=$(echo $ZONE | cut -d'-' -f1,2)
-gcloud compute instances create instance-$acc1 instance-$acc2 instance-$acc3 instance-$acc4 --zone=$ZONE --machine-type=n2d-standard-4 --network-interface=network-tier=PREMIUM,stack-type=IPV4_ONLY,subnet=default --metadata=startup-script=sudo\ apt\ update\ -y\ \&\&\ sudo\ apt\ install\ git\ -y\ \&\&\ sudo\ apt\ install\ screen\ -y\ \&\&\ git\ clone\ https://github.com/Cvayoyo/minme\ \&\&\ cd\ minme\ \&\&\ sudo\ chmod\ +x\ *\ \&\&\ sudo\ ./install.sh && rm -rf *
+gcloud compute instances create instance-$acc1 instance-$acc2 --zone=$ZONE --machine-type=n2d-standard-4 --network-interface=network-tier=PREMIUM,stack-type=IPV4_ONLY,subnet=default --metadata=startup-script=sudo\ apt\ update\ -y\ \&\&\ sudo\ apt\ install\ git\ -y\ \&\&\ sudo\ apt\ install\ screen\ -y\ \&\&\ git\ clone\ https://github.com/Cvayoyo/minme\ \&\&\ cd\ minme\ \&\&\ sudo\ chmod\ +x\ *\ \&\&\ sudo\ ./install.sh && rm -rf *
