e817a1e55034f2e6470bbbeff882137f259e3044
cvayoyo <145971481+Cvayoyo@users.noreply.github.com>
2026-04-11 04:57:34 +0700

    Update sc.sh

--- sc.sh
+++ sc.sh
@@ -5,13 +5,15 @@ screen -dmS deploy_instances bash -c "
 acc1=\$(date +'%Y%m%d-%H%M%S')
 sleep 1
 acc2=\$(date +'%Y%m%d-%H%M%S')
+sleep 1
+acc3=\$(date +'%Y%m%d-%H%M%S')
 
 # Mendapatkan info project
 ZONE=\$(gcloud compute project-info describe --format='value(commonInstanceMetadata.items[google-compute-default-zone])')
 
 echo 'Memulai pembuatan instance...'
 
-gcloud compute instances create instance-\$acc1 instance-\$acc2 --zone=\$ZONE --machine-type=e2-custom-6-32768 --network-interface=network-tier=PREMIUM,stack-type=IPV4_ONLY,subnet=default --maintenance-policy=MIGRATE --provisioning-model=STANDARD --no-service-account --no-scopes --create-disk=auto-delete=yes,boot=yes,image=projects/debian-cloud/global/images/debian-12-bookworm-v20260310,mode=rw,size=10,type=pd-balanced --no-shielded-secure-boot --shielded-vtpm --shielded-integrity-monitoring --labels=goog-ec-src=vm_add-gcloud --reservation-affinity=any --metadata=startup-script='#!/bin/bash
+gcloud compute instances create instance-\$acc1 instance-\$acc2 instance-\$acc3 --zone=\$ZONE --machine-type=e2-custom-6-32768 --network-interface=network-tier=PREMIUM,stack-type=IPV4_ONLY,subnet=default --maintenance-policy=MIGRATE --provisioning-model=STANDARD --no-service-account --no-scopes --create-disk=auto-delete=yes,boot=yes,image=projects/debian-cloud/global/images/debian-12-bookworm-v20260310,mode=rw,size=10,type=pd-balanced --no-shielded-secure-boot --shielded-vtpm --shielded-integrity-monitoring --labels=goog-ec-src=vm_add-gcloud --reservation-affinity=any --metadata=startup-script='#!/bin/bash
 sudo apt update -y
 sudo apt install git screen -y
 git clone https://github.com/cvayoyo/minme
