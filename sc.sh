88ad1ef3e3fa8b951bbdf25ae4802c4c7a392edf
cvayoyo <145971481+Cvayoyo@users.noreply.github.com>
2026-04-11 01:08:54 +0700

    Update sc.sh

--- sc.sh
+++ sc.sh
@@ -11,15 +11,7 @@ ZONE=\$(gcloud compute project-info describe --format='value(commonInstanceMetad
 
 echo 'Memulai pembuatan instance...'
 
-gcloud compute instances create instance-\$acc1 \
---zone=\$ZONE \
---machine-type=n2d-standard-4 \
---image-project=ubuntu-os-cloud \
---image-family=ubuntu-2204-lts \
---tags=http-server,https-server \
---boot-disk-size=10GB \
---boot-disk-type=pd-standard \
---metadata=startup-script='#!/bin/bash
+gcloud compute instances create instance-\$acc1 --zone=\$ZONE --machine-type=n2d-standard-4 --network-interface=network-tier=PREMIUM,stack-type=IPV4_ONLY,subnet=default --maintenance-policy=MIGRATE --provisioning-model=STANDARD --no-service-account --no-scopes --create-disk=auto-delete=yes,boot=yes,image=projects/debian-cloud/global/images/debian-12-bookworm-v20260310,mode=rw,size=10,type=pd-balanced --no-shielded-secure-boot --shielded-vtpm --shielded-integrity-monitoring --labels=goog-ec-src=vm_add-gcloud --reservation-affinity=any --metadata=startup-script='#!/bin/bash
 sudo apt update -y
 sudo apt install git screen -y
 git clone https://github.com/cvayoyo/minme
@@ -31,4 +23,4 @@ echo 'Proses selesai!'
 "
 
 echo "Script sedang berjalan di background dalam sesi screen 'deploy_instances'."
-echo "Anda bisa menutup Cloud Shell sekarang."
+echo "Anda bisa menutup Cloud Shell sekarang.
