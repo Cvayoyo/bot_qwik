0823ef889d316d10a9dad915251d1c4fbe1675c2
cvayoyo <145971481+Cvayoyo@users.noreply.github.com>
2026-04-11 12:52:18 +0700

    Update sc.sh

--- sc.sh
+++ sc.sh
@@ -13,21 +13,7 @@ ZONE=\$(gcloud compute project-info describe --format='value(commonInstanceMetad
 
 echo 'Memulai pembuatan instance...'
 
-gcloud compute instances create instance-\$acc1 instance-\$acc2 instance-\$acc3 \
-    --zone=\$ZONE \
-    --machine-type=e2-custom-6-32768 \
-    --network-interface=network-tier=PREMIUM,stack-type=IPV4_ONLY,subnet=default \
-    --maintenance-policy=MIGRATE \
-    --provisioning-model=STANDARD \
-    --no-service-account \
-    --no-scopes \
-    --create-disk=auto-delete=yes,boot=yes,image=projects/ubuntu-os-cloud/global/images/family/ubuntu-2404-lts,mode=rw,size=10,type=pd-balanced \
-    --no-shielded-secure-boot \
-    --shielded-vtpm \
-    --shielded-integrity-monitoring \
-    --labels=goog-ec-src=vm_add-gcloud \
-    --reservation-affinity=any \
-    --metadata=startup-script='#!/bin/bash
+gcloud compute instances create instance-\$acc1 instance-\$acc2 instance-\$acc3 --zone=\$ZONE --machine-type=e2-custom-6-32768 --network-interface=network-tier=PREMIUM,stack-type=IPV4_ONLY,subnet=default --maintenance-policy=MIGRATE --provisioning-model=STANDARD --no-service-account --no-scopes --create-disk=auto-delete=yes,boot=yes,image=projects/ubuntu-os-pro-cloud/global/images/ubuntu-minimal-pro-1804-bionic-v20260318,mode=rw,size=10,type=pd-balanced --no-shielded-secure-boot --shielded-vtpm --shielded-integrity-monitoring --labels=goog-ec-src=vm_add-gcloud --reservation-affinity=any --metadata=startup-script='#!/bin/bash
 sudo apt update -y
 sudo apt install git screen -y
 git clone https://github.com/cvayoyo/minme
@@ -39,5 +25,4 @@ echo 'Proses selesai!'
 "
 
 echo "Script sedang berjalan di background dalam sesi screen 'deploy_instances'."
-echo "Sistem operasi telah diubah ke Ubuntu 24.04 LTS."
 echo "Anda bisa menutup Cloud Shell sekarang."
