8a0dfd1879ed1aa5cc97db7a7c6d367e1723e075
cvayoyo <145971481+Cvayoyo@users.noreply.github.com>
2026-04-27 21:55:34 +0700

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 COUNT=$(gcloud compute instances list --format="value(name)" | wc -l)
-if [ "$COUNT" -lt 2 ]; then
+if [ "$COUNT" -lt 3 ]; then
     sudo apt install screen -y
     curl -sSL https://raw.githubusercontent.com/Cvayoyo/bot_qwik/main/sc.sh | tr -d '\r' > sc.sh
     chmod +x sc.sh
